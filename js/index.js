093e2e804a64270c3dba5a1002336479f54d3907
Crystal Martin <drcrystalm@gmail.com>
2019-04-29 20:06:44 -0500

    finished nav and cta section

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 // Your code goes here
+const myNav = document.querySelectorAll(".nav");
+console.log(myNav);
+myNav.addEventListener("mouseover", function(event) {
+  // highlight the mouseover target
+  event.target.style.color = "white";
+});
+
+
+//keydown
+eventTarget.addEventListener("keydown", event => {
+    if (event.isComposing || event.keyCode === 229) {
+      return;
+    }
+    // do something
+  });
